0b576ed9573b75dfcc1f2175fa8f1829396cdb98
leticiadtsu <leticiadtsu@gmail.com>
2016-08-14 17:52:43 -0300

    Documentacao models - Parte 3 (Final)

--- JJSoccer/src/models/Sprite.java
+++ JJSoccer/src/models/Sprite.java
@@ -4,18 +4,26 @@ import controller.managers.ImageManager;
 import java.awt.Image;
 
 /**
- *
- * @author
+ * A classe Sprite é a definição das imagens do Actor que será impressa na tela.
+ * Possui como atributo apenas uma image. 
  */
 public class Sprite {
 
     Image imagem;
 
+    /**
+     * Construtor que simplesmente atribui a imagem definida na classe através de uma busca pelas imagens do InputManager.
+     * @param nomeImagem nome da imagem.
+     */
     public Sprite(String nomeImagem) {
             imagem = ImageManager.getInstance().loadImage(nomeImagem);
        
     }
 
+    /**
+     * Retorna a imagem.
+     * @return imagem.
+     */
     public Image getImage() {
         return imagem;
     }
