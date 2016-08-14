7e284405d007f5d7b9e1f585312a3cf705934ba7
leticiadtsu <leticiadtsu@gmail.com>
2016-08-14 15:52:54 -0300

    Documentacao models.basics

--- JJSoccer/src/models/basics/Habilidades.java
+++ JJSoccer/src/models/basics/Habilidades.java
@@ -1,11 +1,15 @@
 package models.basics;
 
 /**
- *
- * @author 
+ * Classe responsável por modelar as habilidades de um jogador. Assim como todas
+ * as classes do pacote basics, não interfere no motor e nem na lógica do jogo.
+ * Define as habilidade, são elas: velocidade, drible, defesa e específica.
  */
 public class Habilidades {
 
+    /**
+     * Criação do enum habilidades.
+     */
     public enum habilidade {
         Velociadade, Drible, Defesa, Especifica;
     }
@@ -15,6 +19,16 @@ public class Habilidades {
     private int defesa;
     private int especifica;
 
+    /**
+     * Construtor das habilidades, atribuit todos os valores recebidos como
+     * parâmetro para o enumerador.
+     *
+     * @param velocidade número que define a velocidade de um jogador.
+     * @param drible número que define a capacidade de drible de um jogador.
+     * @param defesa número que define a capacidade de defesa de um jogador.
+     * @param especial número que define uma habilidade específica de um
+     * jogador.
+     */
     public Habilidades(int velocidade, int drible, int defesa, int especial) {
         this.initVelocidade(velocidade);
         this.initDrible(drible);
@@ -23,7 +37,12 @@ public class Habilidades {
     }
 
     /**
-     * @return the velocidade
+     * Retorna determinada habilidade do jogador de acordo com o tipo desejado
+     * recebido por parâmetro.
+     *
+     * @param tipo habilidade desejada.
+     * @return o número correspondente a habilidade desejada, passada por
+     * parâmetro.
      */
     public int getHabilidade(habilidade tipo) {
         if (tipo == habilidade.Velociadade) {
@@ -39,7 +58,11 @@ public class Habilidades {
     }
 
     /**
-     * @param velocidade the velocidade to set
+     * Método responsável por alterar a velocidade do enumerador habilidades.
+     * Recebe como parâmetro um novo valor, é verificado se está no intervalo
+     * válido e então atribuido
+     *
+     * @param velocidade novo valor para a velocidade.
      */
     private void initVelocidade(int velocidade) {
         if (velocidade > 100) {
@@ -51,7 +74,11 @@ public class Habilidades {
     }
 
     /**
-     * @param drible the drible to set
+     * Método responsável por alterar o drible do enumerador habilidades. Recebe
+     * como parâmetro um novo valor, é verificado se está no intervalo válido e
+     * então atribuido
+     *
+     * @param drible novo valor para a drible.
      */
     private void initDrible(int drible) {
         if (drible > 100) {
@@ -63,7 +90,11 @@ public class Habilidades {
     }
 
     /**
-     * @param defesa the defesa to set
+     * Método responsável por alterar a defesa do enumerador habilidades. Recebe
+     * como parâmetro um novo valor, é verificado se está no intervalo válido e
+     * então atribuido
+     *
+     * @param defesa novo valor para a defesa.
      */
     private void initDefesa(int defesa) {
         if (defesa > 100) {
@@ -75,7 +106,11 @@ public class Habilidades {
     }
 
     /**
-     * @param especial the especial to set
+     * Método responsável por alterar o especial do enumerador habilidades.
+     * Recebe como parâmetro um novo valor, é verificado se está no intervalo
+     * válido e então atribuido
+     *
+     * @param especial novo valor para a especial.
      */
     private void initEspecial(int especial) {
         if (especial > 100) {
@@ -85,5 +120,5 @@ public class Habilidades {
         }
         this.especifica = especial;
     }
-
+    
 }
